ef0d94090e9c9fa352abf92639fe37ea07f4d7fa
PopoBoss <wlad_diaz@yahoo.com>
2022-09-08 11:46:37 -0400

    changes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import { Button, Card, CardGroup, Container, Col, Row, Form } from 'react-bootstrap';
+
+import './movie-view.scss';
 
 export class MovieView extends React.Component {
 
@@ -15,42 +18,57 @@ export class MovieView extends React.Component {
         const { movie, onBackClick } = this.props;
 
         return (
-            <div className='movie-view'>
-                <div className='movie-poster'>
-                    <img src={movie.ImagePath} />
-                </div>
-                <div className='movie-title'>
-                    <span className='label'>Title: </span>
-                    <span className='value'>{movie.Title}</span>
-                </div>
-                <div className='movie-description'>
-                    <span className='label'>Description:</span>
-                    <span className='value'>{movie.Description}</span>
-                </div>
-                <div className='movie-director'>
-                    <span className='label'>Director:</span>
-                    <span className='value'>{movie.Director.Name}</span>
-                </div>
-                <div className='movie-genre'>
-                    <span className='label'>Genre:</span>
-                    <span className='value'>{movie.Genre.Name}</span>
-                </div>
-                <button onClick={() => { onBackClick(null); }}>Back</button>
-            </div>
+            <Container className="movieCardContainer">
+                <Row>
+                    <Col>
+                        <div className="movie-view">
+                            <div className="movie-poster">
+                                <img src={movie.ImagePath} />
+                            </div>
+                            <div className="movie-title">
+                                <span className="label">Title: </span>
+                                <span className="value">{movie.Title}</span>
+                            </div>
+                            <div className="movie-description">
+                                <span className="label">Description: </span>
+                                <span className="value">{movie.Description}</span>
+                            </div>
+                            <div className="movie-releaseyear">
+                                <span className="label">ReleaseYear: </span>
+                                <span className="value">{movie.ReleaseYear}</span>
+                            </div>
+                            <div className="movie-runtime">
+                                <span className="label">RunTime: </span>
+                                <span className="value">{movie.RunTime}</span>
+                            </div>
+                            <div className="movie-director">
+                                <span className="label">Director: </span>
+                                <span className="value">
+                                    {movie.Director.Name +
+                                        ' Bio: ' +
+                                        movie.Director.Bio +
+                                        ' Birth: ' +
+                                        movie.Director.Birth}
+                                </span>
+                            </div>
+                            <div className="movie-genre">
+                                <span className="label">Genre: </span>
+                                <span className="value">
+                                    {movie.Genre.Name + ' Description: ' + movie.Genre.Description}
+                                </span>
+                            </div>
+
+                            <Button
+                                onClick={() => {
+                                    onBackClick(null);
+                                }}
+                            >
+                                Back
+                            </Button>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
-}
-MovieView.propTypes = {
-    movie: PropTypes.shape({
-        Title: PropTypes.string.isRequired,
-        Description: PropTypes.string.isRequired,
-        Genre: PropTypes.shape({
-            Name: PropTypes.string.isRequired
-        }),
-        Director: PropTypes.shape({
-            Name: PropTypes.string.isRequired
-        }),
-        ImageUrl: PropTypes.string.isRequired
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
 };
